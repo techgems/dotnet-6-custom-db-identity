95733f3887842d9ceb8be39e311ee5327fac0f6c
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2022-06-29 16:13:14 -0600

    Make login screen.

---
+++ IdentityCustomDatabase/Components/DashboardFooterViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityCustomDatabase.Components;
+
+public class DashboardFooterViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ IdentityCustomDatabase/Components/DashboardNavbarViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityCustomDatabase.Components;
+
+public class DashboardNavbarViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
