bb0204723efd21b6ce493ac9d8269ec84c22082f
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2022-10-12 16:18:52 -0600

    Begin updating to use nuget package.

--- NotusJS/Views/Dashboard/Navbar/DashboardNavbarComponent.cs
+++ NotusJS/Views/Dashboard/Navbar/DashboardNavbarComponent.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
 using NotusJS.Components;
+using TechGems.RazorComponentTagHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,27 +9,10 @@ using System.Threading.Tasks;
 
 namespace NotusJS.Views.Dashboard.Navbar;
 
-public class DashboardNavbarModel : ServerComponentModel
-{
-}
-
 [HtmlTargetElement("dashboard-navbar")]
-public class DashboardNavbarComponent : ServerComponent
+public class DashboardNavbarComponent : RazorComponentTagHelper
 {
-    public DashboardNavbarComponent(IRazorRenderer razorRenderer) : base(razorRenderer)
+    public DashboardNavbarComponent() : base("~/Views/Dashboard/Navbar/Navbar.cshtml")
     {
-    }
-
-    //[HtmlAttributeName("background")]
-    //public string BackgroundImageLink { get; set; } = string.Empty;
-
-    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
-    {
-        var model = new DashboardNavbarModel()
-        {
-
-        };
-
-        await RenderPartialView("~/Views/Dashboard/Navbar/Navbar.cshtml", output, model);
     }
 }
