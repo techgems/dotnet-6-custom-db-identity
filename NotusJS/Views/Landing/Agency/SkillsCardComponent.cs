77a907619ad77d7e281e1a251a85494577c0eb0c
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2022-11-01 23:13:08 -0600

    Add agency components and use latest version of RazorComponentTagHelpers.

---
+++ NotusJS/Views/Landing/Agency/SkillsCardComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechGems.RazorComponentTagHelpers;
+
+namespace NotusJS.Views.Landing.Agency;
+
+[HtmlTargetElement("landing-skills-card")]
+public class SkillsCardComponent : RazorComponentTagHelper
+{
+    public static readonly string DescriptionSlot = "descriptionSlot";
+    public static readonly string HeaderSlot = "headerSlot";
+
+    public SkillsCardComponent() : base("~/Views/Landing/Agency/SkillsCard.cshtml")
+    {
+
+    }
+
+    [HtmlAttributeName("top-align-class")]
+    public string TopAlignmentClass { get; set; } = "";
+
+    [HtmlAttributeName("icon-bg-color")]
+    public string IconBgColor { get; set; } = "";
+
+    [HtmlAttributeName("fab-icon-class")]
+    public string FabIconClass { get; set; } = "";
+}
