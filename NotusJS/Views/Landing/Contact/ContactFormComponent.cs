260db7627cfe3f7ea2851ba5c78d82bdd185cfe0
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2022-11-03 22:48:02 -0600

    Replace normal html with components.

---
+++ NotusJS/Views/Landing/Contact/ContactFormComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechGems.RazorComponentTagHelpers;
+
+namespace NotusJS.Views.Landing.Contact;
+
+[HtmlTargetElement("contact-form")]
+public class ContactFormComponent : RazorComponentTagHelper
+{
+    public ContactFormComponent() : base("~/Views/Landing/Contact/ContactForm.cshtml")
+    {
+    }
+}
---
+++ NotusJS/Views/Landing/Contact/ServicesCardComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechGems.RazorComponentTagHelpers;
+
+namespace NotusJS.Views.Landing.Contact;
+
+[HtmlTargetElement("services-card")]
+public class ServicesCardComponent : RazorComponentTagHelper
+{
+    public ServicesCardComponent() : base("~/Views/Landing/Contact/ServicesCard.cshtml")
+    {
+    }
+
+    [HtmlAttributeName("name")]
+    public string Name { get; set; } = "";
+
+    [HtmlAttributeName("description")]
+    public string Description { get; set; } = "";
+}
